d7a81e78015b8fe886ac9e7fd18a0bba28d0c3dc
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-05-26 14:27:31 -0300

    Criando função __STR__ na classe do CPF

--- python/cursosBase/curso07/Cpf.py
+++ python/cursosBase/curso07/Cpf.py
@@ -1,7 +1,7 @@
 class Cpf:
     def __init__(self, document):
         if self.validated_cpf(document):
-            self.cpf = str(document)
+            self._cpf = str(document)
         else:
             raise ValueError("CPF inválido!")
         self.format_cpf()
@@ -13,8 +13,11 @@ class Cpf:
             return False
 
     def format_cpf(self):
-        first_part = self.cpf[:3]
-        second_part = self.cpf[3:6]
-        third_part = self.cpf[6:9]
-        fourth_part = self.cpf[9:]
-        self.formated_cpf = "{}.{}.{}-{}".format(first_part, second_part, third_part, fourth_part)
+        first_part = self._cpf[:3]
+        second_part = self._cpf[3:6]
+        third_part = self._cpf[6:9]
+        fourth_part = self._cpf[9:]
+        self._formated_cpf = "{}.{}.{}-{}".format(first_part, second_part, third_part, fourth_part)
+
+    def __str__(self):
+        return 'O CPF é: {}'.format(self._formated_cpf)
--- python/cursosBase/curso07/main.py
+++ python/cursosBase/curso07/main.py
@@ -3,5 +3,4 @@ from Cpf import Cpf
 cpf = 15151515111
 
 cpf1 = Cpf(cpf)
-print(cpf1.cpf)
-print(cpf1.formated_cpf)
+print(cpf1)
