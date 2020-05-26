0239031da15daf88cc3b21add42b3c438bf20f42
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-05-26 12:05:08 -0300

    Tratando erros com a conexão com o servidor

--- python/Calculadora_RPC/client.py
+++ python/Calculadora_RPC/client.py
@@ -5,7 +5,14 @@ multicall = xmlrpc.client.MultiCall(proxy)
 chose = 0
 print("Bem vindo a calculadora remota")
 
-print(multicall)
+try:
+    multicall.connection(1)
+    result = multicall()
+    result = list(result)
+    print(result[0])
+except:
+    print("Ocorreu um erro! Verifique a conexão com o servidor.")
+    exit()
 
 while(chose != 6):
     print("Digite: \n1-Soma 2-Subtração 3-Multiplicação 4-Divisão 5-Potenciação 6-Sair:")
@@ -17,19 +24,19 @@ while(chose != 6):
     multicall.subtract(number1, number2)
     multicall.multiply(number1, number2)
     multicall.divide(number1, number2)
-    multicall.potetiation(number1, number2)
+    multicall.potentiation(number1, number2)
     result = multicall()
     result = list(result)
 
     if(chose == 1):
-        print("O valor da soma entre {} e {} é: {}".format(number1, number2, result[0]))
+        print("O valor da soma entre {} e {} é: {}".format(number1, number2, result[chose]))
     elif(chose == 2):
-        print("O valor da subtração entre {} e {} é: {}".format(number1, number2, result[1]))
+        print("O valor da subtração entre {} e {} é: {}".format(number1, number2, result[chose]))
     elif(chose == 3):
-        print("O valor da multiplicação entre {} e {} é: {}".format(number1, number2, result[2]))
+        print("O valor da multiplicação entre {} e {} é: {}".format(number1, number2, result[chose]))
     elif(chose == 4):
-        print("O valor da divisão entre {} e {} é: {}".format(number1, number2, result[3]))
+        print("O valor da divisão entre {} e {} é: {}".format(number1, number2, result[chose]))
     elif(chose == 5):
-        print("O valor da potenciação entre {} e {} é: {}".format(number1, number2, result[4]))
+        print("O valor da potenciação entre {} e {} é: {}".format(number1, number2, result[chose]))
     elif(chose != 6):
         chose = 0
--- python/Calculadora_RPC/server.py
+++ python/Calculadora_RPC/server.py
@@ -15,12 +15,16 @@ def divide(x, y):
 def potentiation(x,y):
     return x ** y
 
+def connection(x):
+    return "Conexão realizada com sucesso!"
+
 server = SimpleXMLRPCServer(("localhost", 8000))
 print("Listening on port 8000...")
 server.register_multicall_functions()
-server.register_function(add, 'add')
-server.register_function(subtract, 'subtract')
-server.register_function(multiply, 'multiply')
-server.register_function(divide, 'divide')
-server.register_function(potentiation, 'potentiation')
+server.register_function(add, "add")
+server.register_function(subtract, "subtract")
+server.register_function(multiply, "multiply")
+server.register_function(divide, "divide")
+server.register_function(potentiation, "potentiation")
+server.register_function(connection, "connection")
 server.serve_forever()
