b1dfa893e5e5f6f4af928bdb167d2fd977b436bc
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-05-26 11:48:16 -0300

    Criado a calculadora e começando a melhorar ela

--- python/Calculadora_RPC/client.py
+++ python/Calculadora_RPC/client.py
@@ -1,5 +1,35 @@
-import xmlrpclib
+import xmlrpc.client
 
-server = xmlrpclib.ServerProxy("http://localhost:8888")
+proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+multicall = xmlrpc.client.MultiCall(proxy)
+chose = 0
+print("Bem vindo a calculadora remota")
 
-print server.now()
+print(multicall)
+
+while(chose != 6):
+    print("Digite: \n1-Soma 2-Subtração 3-Multiplicação 4-Divisão 5-Potenciação 6-Sair:")
+    chose = int(input())
+    number1 = int(input('Digite o primeiro número da operação: '))
+    number2 = int(input('Digite o segundo número da operação: '))
+
+    multicall.add(number1, number2)
+    multicall.subtract(number1, number2)
+    multicall.multiply(number1, number2)
+    multicall.divide(number1, number2)
+    multicall.potetiation(number1, number2)
+    result = multicall()
+    result = list(result)
+
+    if(chose == 1):
+        print("O valor da soma entre {} e {} é: {}".format(number1, number2, result[0]))
+    elif(chose == 2):
+        print("O valor da subtração entre {} e {} é: {}".format(number1, number2, result[1]))
+    elif(chose == 3):
+        print("O valor da multiplicação entre {} e {} é: {}".format(number1, number2, result[2]))
+    elif(chose == 4):
+        print("O valor da divisão entre {} e {} é: {}".format(number1, number2, result[3]))
+    elif(chose == 5):
+        print("O valor da potenciação entre {} e {} é: {}".format(number1, number2, result[4]))
+    elif(chose != 6):
+        chose = 0
--- python/Calculadora_RPC/server.py
+++ python/Calculadora_RPC/server.py
@@ -1,8 +1,26 @@
-import SimpleXMLRPCServer
-from datetime import datetime
+from xmlrpc.server import SimpleXMLRPCServer
 
-server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8888))
-server.register_instance(datetime)
+def add(x, y):
+    return x + y
 
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x // y
+
+def potentiation(x,y):
+    return x ** y
+
+server = SimpleXMLRPCServer(("localhost", 8000))
+print("Listening on port 8000...")
+server.register_multicall_functions()
+server.register_function(add, 'add')
+server.register_function(subtract, 'subtract')
+server.register_function(multiply, 'multiply')
+server.register_function(divide, 'divide')
+server.register_function(potentiation, 'potentiation')
 server.serve_forever()
-
