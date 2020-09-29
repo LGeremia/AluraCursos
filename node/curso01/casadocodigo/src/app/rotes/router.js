c025c2a5218a3ba9d6a4d1547c4eb427619d3ec7
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-09-28 22:14:38 -0300

    adicionando conexão ao Banco de Dados

--- node/curso01/casadocodigo/src/app/rotes/router.js
+++ node/curso01/casadocodigo/src/app/rotes/router.js
@@ -1,21 +1,31 @@
+const db = require('../../config/database.js');
+const listaMarko = require('../views/livros/lista/lista.marko.js');
+const LivroDao = require('../infra/livro-dao');
+
 module.exports = (app) => {
     app.get(`/`, function(req, res){
         res.send(
             `<html>
                 <body>
-                    <h1>teste</h1>
+                    <h1>teste nodemon</h1>
                 </body>
             </html>`
         );
     });
 
     app.get(`/livros`, function(req, res){
-        res.send(
-            `<html>
-                <body>
-                    <h1>Livros</h1>
-                </body>
-            </html>`
-        );
+        const livroDao = new LivroDao(db);
+        livroDao.lista(function(erro, resultados) {
+
+            res.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+    
+            );
+    
+        });
+        
     });
 }
